d9a96540d79661c1713230466cfb286ae9e0d593
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-05-18 01:17:47 +0530

    Initial commit

---
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -0,0 +1,39 @@
+// Rewrite the factorial function using a `while` loop.
+pub fn factorial(mut n: u32) -> u32 {
+    // The `todo!()` macro is a placeholder that the compiler
+    // interprets as "I'll get back to this later", thus
+    // suppressing type errors.
+    // It panics at runtime.
+    let mut sum: u32 = 1;
+    while(n > 0) {
+        sum = sum * n;
+        n = n - 1;
+    }
+
+    return sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::factorial;
+
+    #[test]
+    fn first() {
+        assert_eq!(factorial(0), 1);
+    }
+
+    #[test]
+    fn second() {
+        assert_eq!(factorial(1), 1);
+    }
+
+    #[test]
+    fn third() {
+        assert_eq!(factorial(2), 2);
+    }
+
+    #[test]
+    fn fifth() {
+        assert_eq!(factorial(5), 120);
+    }
+}
